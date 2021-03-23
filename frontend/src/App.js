851d07c844000f68826de75e35ebedd576d0ed54
happyssun96 <lsw96@naver.com>
2021-03-24 00:43:58 +0900

    ADD : menubarComponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import Header from './component/Header';
-import Recommend from './component/Recommend';
-import Rutin from './component/Rutin';
+import "./App.css";
+import Header from "./component/Header";
+import Recommend from "./component/Recommend";
+import Rutin from "./component/Rutin";
+import Menubar from "./component/Menubar";
+
 function App() {
   return (
     <>
       <Header />
       <Recommend />
       <Rutin />
+      <Menubar />
     </>
   );
 }
--- frontend/src/component/NavBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NavBar() {
-  return <>.navbar</>;
-}
-
-export default NavBar;
