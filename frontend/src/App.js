74fba88a238464688ee918ef5006cc1d8c584376
sangzun-han <hsjun1996@naver.com>
2021-04-01 15:28:02 +0900

    ADD : workout.js 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import Home from "./component/Home";
-import Mypage from "./component/Mypage";
-import Exercise from "./component/Exercise";
+import Home from './component/Home';
+import Mypage from './component/Mypage';
+import Exercise from './component/Exercise';
+import Workout from './component/Workout';
 
-import "./css/App.css";
+import './css/App.css';
 
 function App() {
   return (
     <div>
-      <Route path="/" exact="true" component={Home} />
-      <Route path="/mypage" component={Mypage} />
-      <Route path="/exercise" component={Exercise} />
+      <Route path='/' exact='true' component={Home} />
+      <Route path='/mypage' component={Mypage} />
+      <Route path='/exercise' component={Exercise} />
+      <Route path='/workout' component={Workout} />
     </div>
   );
 }
