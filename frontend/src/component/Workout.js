ca71ad63246fa9b34eeada496ce285f7d960634a
sangzun-han <hsjun1996@naver.com>
2021-04-03 08:19:36 +0900

    FIX : workout.js 디자인 변경

--- frontend/src/component/Workout.js
+++ frontend/src/component/Workout.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import '../css/workout.css';
 function Workout() {
-  return <div></div>;
+  const date = new Date();
+  const [exerlist, setExerlist] = useState(['pushup', 'pushup', 'pushup']);
+
+  return (
+    <div className='explainWrap'>
+      <div className='explainFlex'>
+        <div className='explain'>
+          <p>
+            {date.getFullYear()}년&nbsp;
+            {date.getMonth() + 1}월&nbsp;
+            {date.getDate()}일
+          </p>
+          <p>1. 주의사항 적기</p>
+          <p>2. 주의사항 적기</p>
+          <p>3. 주의사항 적기</p>
+          <p>4. 화면에 신체가 잘 인식되도록 서주세요!</p>
+          <button>운동시작!</button>
+        </div>
+
+        <div className='exerlistFlex'>
+          {exerlist.map((a, i) => {
+            console.log(a);
+            return (
+              <div className='exerList' key={i}>
+                <img src={'img/' + a + '.gif'} alt='img' />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Workout;
