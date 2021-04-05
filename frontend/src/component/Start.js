4a409189ec2a7d856fdf52091554dbb09b06ab26
sangzun-han <hsjun1996@naver.com>
2021-04-05 19:13:54 +0900

    start.js count디자인,record기능추가

--- frontend/src/component/Start.js
+++ frontend/src/component/Start.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Webcam from 'react-webcam';
-
+import WebcamStreamCapture from './WebcamStreamCapture';
 import '../css/App.css';
 import '../css/start.css';
 
@@ -17,21 +17,32 @@ const WebCamComponent = () => (
     videoConstraints={videoConstraints}
     style={{
       objectFit: 'fill',
+      position: 'relative',
     }}
   />
 );
-
 function Start() {
   return (
     <div className='startWrap'>
       <div className='container'>
         <WebCamComponent />
+
+        <div className='output'>
+          <div className='count'>
+            <span>2개</span>
+          </div>
+        </div>
+
         <div className='exampleView'>
           <div className='examExer'>
             <img src='/img/pushup.gif' alt='img' />
           </div>
           <div className='tts'>
             <p>tts 내용</p>
+            <div className='button'>
+              <button class='finish'>끝내기</button>
+              <WebcamStreamCapture className='record' />
+            </div>
           </div>
         </div>
       </div>
