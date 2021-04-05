a1c2376b056c8593e817f8a5ff70626f1b4d103f
happyssun96 <lsw96@naver.com>
2021-04-05 15:58:19 +0900

    FIX : Login.js, Register.js 및 css 변경

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import { Link, Redirect } from "react-router-dom";
 import Menubar from "./Menubar";
 import "../css/Login.css";
+
 /* 로그인 Back-End 부분 => 추가 라이브러리를 설치해야 가능
 import Axios from 'axios'
 import { response } from 'express'
@@ -10,19 +13,25 @@ import { useDispatch } from 'react-redux'
 import { loginUser } from '../../../_actions/user_action'
 */
 
-/*
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
       margin: theme.spacing(1),
+      /*링크 밑줄 없애기*/
+      textDecoration: "none",
+    },
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
     },
   },
 }));
-*/
+
 const Login = () => {
   // 로그인 Back-End 부분
   // const dispatch = useDispatch();
 
+  const classes = useStyles();
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
 
@@ -55,24 +64,33 @@ const Login = () => {
       <Menubar />
       <h2 style={{ textAlign: "center" }}>Login</h2>
       <div className="login__flex">
-        <form
-          className="loginForm"
-          style={{ display: "flex", flexDirection: "column" }}
-        >
-          <label>Email</label>
-          <input type="email" value={Email} onChange={onEmailHandler} />
-          <label>Password</label>
-          <input
-            type="password"
-            value={Password}
-            onChange={onPasswordHandler}
-          />
-
-          <br />
-          <div>
-            <button>로그인</button>
+        <form style={{ display: "flex", flexDirection: "column" }}>
+          <div className={classes.root}>
+            <TextField
+              label="이메일"
+              variant="outlined"
+              size="small"
+              type="email"
+              value={Email}
+              onChange={onEmailHandler}
+            />
+            <br />
+            <TextField
+              label="비밀번호"
+              type="password"
+              variant="outlined"
+              size="small"
+              value={Password}
+              onChange={onPasswordHandler}
+            />
+            <br />
+            <Button variant="contained" color="primary">
+              로그인
+            </Button>
             <Link to="/register">
-              <button>회원가입</button>
+              <Button variant="contained" color="secondary">
+                회원가입
+              </Button>
             </Link>
           </div>
         </form>
--- frontend/src/component/Register.js
+++ frontend/src/component/Register.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import { Link, Redirect } from "react-router-dom";
 import Menubar from "./Menubar";
 import "../css/Register.css";
+import "../css/App.css";
 // 회원가입 Back-End 부분 => 추가 라이브러리 설치해야 가능
 /*
 import Axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { loginUser } from '../../../_actions/user_action'
 */
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
 const Register = () => {
   // 로그인 Back-End 부분
   // const dispatch = useDispatch();
-
+  const classes = useStyles();
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
   const [Name, setName] = useState("");
@@ -60,30 +75,48 @@ const Register = () => {
       <Menubar />
       <h2 style={{ textAlign: "center" }}>Register</h2>
       <div className="register__flex">
-        <form
-          className="registerForm"
-          style={{ display: "flex", flexDirection: "column" }}
-        >
-          <label>Email</label>
-          <input type="email" value={Email} onChange={onEmailHandler} />
-          <label>Name</label>
-          <input type="text" value={Name} onChange={onNameHandler} />
-
-          <label>Password</label>
-          <input
-            type="password"
-            value={Password}
-            onChange={onPasswordHandler}
-          />
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            value={ConfirmPassword}
-            onChange={onConfirmPasswordHandler}
-          />
-
-          <br />
-          <button>회원가입</button>
+        <form style={{ display: "flex", flexDirection: "column" }}>
+          <div className={classes.root}>
+            <TextField
+              label="이메일"
+              variant="outlined"
+              size="small"
+              type="email"
+              value={Email}
+              onChange={onEmailHandler}
+            />
+            <br />
+            <TextField
+              label="이름"
+              variant="outlined"
+              size="small"
+              type="name"
+              value={Name}
+              onChange={onNameHandler}
+            />
+            <br />
+            <TextField
+              label="비밀번호"
+              type="password"
+              variant="outlined"
+              size="small"
+              value={Password}
+              onChange={onPasswordHandler}
+            />
+            <br />
+            <TextField
+              label="비밀번호 확인"
+              type="password"
+              variant="outlined"
+              size="small"
+              value={ConfirmPassword}
+              onChange={onConfirmPasswordHandler}
+            />
+            <br />
+            <Button variant="contained" color="secondary">
+              회원가입
+            </Button>
+          </div>
         </form>
       </div>
     </div>
