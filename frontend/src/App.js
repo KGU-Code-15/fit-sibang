7d3fdb8ffa707b88974098a9782b60835ea54f27
sangzun-han <hsjun1996@naver.com>
2021-04-21 09:06:11 +0900

    ADD : teachableMachine 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import Home from './component/Home'
-import Mypage from './component/Mypage'
-import Exercise from './component/Exercise'
-import Workout from './component/Workout'
-import Start from './component/Start'
-import Login from './component/Login'
-import Register from './component/Register'
-import './css/App.css'
+import Home from './component/Home';
+import Mypage from './component/Mypage';
+import Exercise from './component/Exercise';
+import Workout from './component/Workout';
+import Login from './component/Login';
+import Register from './component/Register';
+import TeachableMachine from './component/TeachableMachine';
+import './css/App.css';
 
 function App() {
   return (
     <div>
-      <Route path="/" exact="true" component={Home} />
-      <Route path="/mypage" component={Mypage} />
-      <Route path="/exercise" component={Exercise} />
-      <Route path="/workout" component={Workout} />
-      <Route path="/start" component={Start} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
+      <Route path='/' exact='true' component={Home} />
+      <Route path='/mypage' component={Mypage} />
+      <Route path='/exercise' component={Exercise} />
+      <Route path='/workout' component={Workout} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+      <Route path='/TeachableMachine' component={TeachableMachine} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
