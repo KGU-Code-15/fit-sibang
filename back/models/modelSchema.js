6bee805e3b343e7a0ee1123d6354ad3209057435
DESKTOP-ORQLQSL\YIM <gksals6464@kyonggi.ac.kr>
2021-04-25 23:11:58 +0900

    ADD: 비밀번호 암호화

--- back/models/modelSchema.js
+++ back/models/modelSchema.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt') // 암호화
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
   userName: {
@@ -37,6 +39,22 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+})
+
+// save함수의 전처리
+userSchema.pre('save', function (next) {
+  var user = this
+  if (user.isModified('password')) {
+    // 비밀번호 변경시의 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err)
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err)
+        user.password = hash
+        next()
+      })
+    })
+  }
 })
 
 const User = mongoose.model('User', userSchema)
