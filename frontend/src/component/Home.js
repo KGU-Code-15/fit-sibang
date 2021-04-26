6a859fccf3aa33a44a170e5ac639f49cede2de00
DESKTOP-ORQLQSL\YIM <gksals6464@kyonggi.ac.kr>
2021-04-27 00:52:41 +0900

    ADD: 로그인 로그아웃 버튼 구현

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Youtube from 'react-youtube';
-import axios from 'axios';
+import React from 'react'
+import Youtube from 'react-youtube'
+import axios from 'axios'
 
-import Menubar from './Menubar';
-import Main from './Main';
-import '../css/App.css';
-import { withRouter } from 'react-router-dom';
+import Menubar from './Menubar'
+import Main from './Main'
+import '../css/App.css'
+import { withRouter } from 'react-router-dom'
 
 function Home() {
   const opts = {
@@ -14,28 +14,28 @@ function Home() {
     playerVars: {
       autoplay: 0,
     },
-  };
+  }
 
   function _onReady(e) {
-    e.target.pauseVideo();
+    e.target.pauseVideo()
   }
 
   return (
-    <div className='wrap'>
+    <div className="wrap">
       <div>
         <Menubar />
       </div>
       <div>
         <Main />
         <Youtube
-          className='youtube'
-          videoId='qHapWanGDc8'
+          className="youtube"
+          videoId="qHapWanGDc8"
           opts={opts}
           onReady={_onReady}
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default withRouter(Home);
+export default withRouter(Home)
