78817c7c834a59a941519a9d89c34c91e5ccfc08
happyssun96 <lsw96@naver.com>
2021-04-27 01:06:35 +0900

    Fix : 로고 이미지 추가 및 UI 수정

--- frontend/src/component/ExerciseSection.js
+++ frontend/src/component/ExerciseSection.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import '../css/Exercise.css'
+import React from "react";
+import "../css/Exercise.css";
+import { Route, Link, withRouter } from "react-router-dom";
 
 function ExerciseSection() {
   return (
@@ -7,15 +8,17 @@ function ExerciseSection() {
       <div className="rutin_page">
         <div className="rutin_pageFlex">
           <div className="rutin_pageInfo">
-            <span>운동 목록</span>
+            <span>원하시는 운동을 선택하세요!</span>
             <div className="rutin__img">
               <div className="rutin__wrap">
                 <img src="/img/spinal_twist.png" alt="health__image" />
                 <p>요가 입문을 위한 기본 자세</p>
               </div>
               <div className="rutin__wrap">
-                <img src="/img/squat.jpg" alt="health__image" />
-                <p>25분 칼로리 버닝! 타바타 클래스</p>
+                <Link to="/TeachableMachine">
+                  <img src="/img/squat.jpg" alt="health__image" />
+                  <p>25분 칼로리 버닝! 타바타 클래스</p>
+                </Link>
               </div>
               <div className="rutin__wrap">
                 <img src="/img/25.corpse-pose.png" alt="health__image" />
@@ -26,19 +29,19 @@ function ExerciseSection() {
                 <p>하루 10분 플랭크!</p>
               </div>
               <div className="rutin__wrap">
-                <img src="/img/squat.jpg" alt="health__image" />
-                <p>25분 칼로리 버닝! 타바타 클래스</p>
+                <img src="/img/lunge.png" alt="health__image" />
+                <p>가볍게 시작하는 런지</p>
               </div>
               <div className="rutin__wrap">
-                <img src="/img/squat.jpg" alt="health__image" />
-                <p>25분 칼로리 버닝! 타바타 클래스</p>
+                <img src="/img/childPose.jpg" alt="health__image" />
+                <p>스트레스를 풀어주는 요가 자세 모음</p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ExerciseSection
+export default ExerciseSection;
--- frontend/src/component/Menubar.js
+++ frontend/src/component/Menubar.js
@@ -1,51 +1,73 @@
-import React from 'react';
-import { Route, Link, withRouter } from 'react-router-dom';
-import axios from 'axios';
-import '../css/App.css';
+import React, { useState } from "react";
+import { Route, Link, withRouter } from "react-router-dom";
+import axios from "axios";
+import "../css/App.css";
+
+import { useDispatch } from "react-redux";
 
 function Menubar(props) {
-  //logout handler
+  const dispatch = useDispatch();
+  let auth_login;
+  const [text, setText] = useState("로그인");
+  axios.get(`/user/auth`).then((response) => {
+    if (response.data.isAuth) {
+      auth_login = true; // 현 상태 : login
+      let textCopy = [...text];
+      textCopy = "로그아웃";
+      setText(textCopy);
+    } else {
+      auth_login = false; // 현 상태 : logout
+      let textCopy = [...text];
+      textCopy = "로그인";
+      setText(textCopy);
+    }
+  });
   const onClickHandler = () => {
-    axios.get(`/user/logout`).then((response) => {
-      if (response.data.success) {
-        props.history.push('/login');
-      } else {
-        alert('로그인 안했음');
-      }
-    });
+    if (auth_login) {
+      axios.get(`/user/logout`).then((response) => {
+        if (response.data.success) {
+          props.history.push("/login");
+        } else {
+          alert("로그인 안했음");
+        }
+      });
+    } else {
+      props.history.push("/login");
+    }
   };
-  return (
-    <div className='sidebox'>
-      <div className='menuLogo'>
-        <span>핏-</span>
-        <span className='title'>시방</span>
-      </div>
 
-      <div className='menuFlex'>
-        <div className='menuListF'>
-          <Link to='/'>
+  return (
+    <div className="sidebox">
+      <div className="menuLogo">
+        <Link to="/">
+          <img id="logoImg" src="/img/logo.png" alt="loco" />
+        </Link>
+      </div>
+      <div className="menuFlex">
+        <div className="menuListF">
+          <Link to="/">
             <span>홈</span>
           </Link>
         </div>
-        <div className='menuList'>
+        <div className="menuList">
           <span>챌린지</span>
         </div>
-        <div className='menuList'>
-          <Link to='/exercise'>
+        <div className="menuList">
+          <Link to="/exercise">
             <span>운동</span>
           </Link>
         </div>
-        <div className='menuList'>
+        <div className="menuList">
           <span>상품</span>
         </div>
-        <div className='menuList'>
-          <Link to='mypage'>
+        <div className="menuList">
+          <Link to="mypage">
             <span>마이페이지</span>
           </Link>
         </div>
-        <div className='menuListL'>
-          <button className='logout' onClick={onClickHandler}>
-            <span>로그아웃</span>
+        <div className="menuListL">
+          <button className="logout" onClick={onClickHandler}>
+            <span>{text}</span>
           </button>
         </div>
       </div>
