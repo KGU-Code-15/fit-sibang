eccdcc4c87b7ee9f3f686586ab53890afd5d4772
sangzun-han <hsjun1996@naver.com>
2021-04-27 01:12:42 +0900

    FIX : 디자인변경 + 로그인/로그아웃 분기

---
+++ frontend/src/component/ExerciseData.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import '../css/App.css';
+import '../css/Main.css';
+
+function ExerciseData() {
+  return (
+    <>
+      <div className='exer'>
+        <span className='sport'>요가</span>
+        <span className='time'>03:50</span>
+        <span className='compare'>02</span>
+      </div>
+      <div className='exer'>
+        <span className='sport'>푸쉬업</span>
+        <span className='time'>13:50</span>
+        <span className='compare'>02</span>
+      </div>
+      <div className='exer'>
+        <span className='sport'>싯업</span>
+        <span className='time'>02:25</span>
+        <span className='compare'>02</span>
+      </div>
+      <div className='exer'>
+        <span className='sport'>스쿼트</span>
+        <span className='time'>6:50</span>
+        <span className='compare'>02</span>
+      </div>
+    </>
+  );
+}
+
+export default ExerciseData;
--- frontend/src/component/Menubar.js
+++ frontend/src/component/Menubar.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Link, withRouter } from 'react-router-dom';
 import axios from 'axios';
 import '../css/App.css';
 
+import { useDispatch } from 'react-redux';
+
 function Menubar(props) {
-  //logout handler
+  const dispatch = useDispatch();
+  let auth_login;
+  const [text, setText] = useState('로그인');
+  axios.get(`/user/auth`).then((response) => {
+    if (response.data.isAuth) {
+      auth_login = true; // 현 상태 : login
+      let textCopy = [...text];
+      textCopy = '로그아웃';
+      setText(textCopy);
+    } else {
+      auth_login = false; // 현 상태 : logout
+      let textCopy = [...text];
+      textCopy = '로그인';
+      setText(textCopy);
+    }
+  });
   const onClickHandler = () => {
-    axios.get(`/user/logout`).then((response) => {
-      if (response.data.success) {
-        props.history.push('/login');
-      } else {
-        alert('로그인 안했음');
-      }
-    });
+    if (auth_login) {
+      axios.get(`/user/logout`).then((response) => {
+        if (response.data.success) {
+          props.history.push('/login');
+        } else {
+          alert('로그인 안했음');
+        }
+      });
+    } else {
+      props.history.push('/login');
+    }
   };
+
   return (
     <div className='sidebox'>
       <div className='menuLogo'>
@@ -45,7 +67,7 @@ function Menubar(props) {
         </div>
         <div className='menuListL'>
           <button className='logout' onClick={onClickHandler}>
-            <span>로그아웃</span>
+            <span>{text}</span>
           </button>
         </div>
       </div>
