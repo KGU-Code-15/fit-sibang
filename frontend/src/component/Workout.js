f39822e9596972e06558738f504e06d5461a98be
happyssun96 <lsw96@naver.com>
2021-05-04 16:56:12 +0900

    FIX : Workout페이지 디자인 수정

--- frontend/src/component/Workout.js
+++ frontend/src/component/Workout.js
@@ -24,10 +24,10 @@ function Workout({ history }) {
             {date.getMonth() + 1}월&nbsp;
             {date.getDate()}일
           </p>
-          <p>1. 카메라 권한 허용해주세요.</p>
+          <p>1. 카메라 권한을 허용해주세요.</p>
           <p>2. 디바이스를 사용자 앞으로 놓아주세요.</p>
           <p>3. 화면에 신체가 잘 인식되도록 서주세요.</p>
-          <p>4. 카운터와 사용자 카메라가 실행되면 '운동하기'를 클릭하세요.</p>
+          <p>4. '운동하기'를 클릭해 즐거운 운동하세요!</p>
           <Link to="TeachableMachine">
             <button>운동하기</button>
           </Link>
