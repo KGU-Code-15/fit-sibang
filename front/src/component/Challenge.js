1f2cc314b89e73c51962007925fbd53f5c61bac6
d9249 <d9249@kyonggi.ac.kr>
2021-05-06 04:02:16 +0900

    ADD : Challenge page.

--- front/src/component/Challenge.js
+++ front/src/component/Challenge.js
@@ -1,10 +1,28 @@
 import React from "react"
 import "../css/Challenge.css"
+import TopHeader from "./TopHeader"
 
 export default function Challenge() {
   return (
-    <div>
-      <div>Challenge</div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <TopHeader />
+      <div className="">
+        <div className="ChallText">Fit-sibang과 함께하는 첫번째 Challenge</div>
+        <img className="ChallImg" src="./Challengeimg/Flank.jpg" />
+        <div className="ChallText">Fit-sibang과 함께하는 두번째 Challenge</div>
+        <img className="ChallImg" src="./Challengeimg/Pull-up.jpg" />
+        <div className="ChallText">Fit-sibang과 함께하는 세번째 Challenge</div>
+        <img className="ChallImg" src="./Challengeimg/Push-up.jpg" />
+        <div className="ChallText">Fit-sibang과 함께하는 네번째 Challenge</div>
+        <img className="ChallImg" src="./Challengeimg/Squat.jpg" />
+      </div>
     </div>
   )
 }
