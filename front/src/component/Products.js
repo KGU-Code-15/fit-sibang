d0fcf20c2ead1afc1afdb5c59ed0f272aaf092c1
hanminssssssss <60065661+hanminssssssss@users.noreply.github.com>
2021-05-06 22:30:25 +0900

    fix: err catch

--- front/src/component/Products.js
+++ front/src/component/Products.js
@@ -1,43 +1,42 @@
-import React from "react"
-import "../css/Products.css"
-import TopHeader from "./TopHeader"
-import { Route, Link, withRouter } from "react-router-dom"
-import { ProductData } from "./ProductData"
-import Footer from "./Footer"
+import React from 'react'
+import '../css/Products.css'
+import TopHeader from './TopHeader'
+import { ProductData } from './ProductData'
+import Footer from './Footer'
 
 export default function Products(props) {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <TopHeader />
       {ProductData.map((item, index) => {
         return (
           <div key={index} className={item.cName}>
-            <div class="wrapper">
-              <div class="card">
-                <div class="product-left">
-                  <div class="header">
+            <div className="wrapper">
+              <div className="card">
+                <div className="product-left">
+                  <div className="header">
                     <h2>
                       {item.number}, {item.title}
                     </h2>
                   </div>
-                  <div class="product-main">
+                  <div className="product-main">
                     <p>{item.explanation}</p>
                   </div>
-                  <div class="product-details">
-                    <div class="product-total">
+                  <div className="product-details">
+                    <div className="product-total">
                       <h3>{item.price}</h3>
                     </div>
                   </div>
                 </div>
-                <div class="product-right">
-                  <img src={item.image} />
+                <div className="product-right">
+                  <img src={item.image} alt="err" />
                 </div>
               </div>
             </div>
