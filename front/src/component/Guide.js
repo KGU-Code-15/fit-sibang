b4506091cc12b189200bd9afaab25426da6486ad
d9249 <d9249@kyonggi.ac.kr>
2021-05-25 20:29:22 +0900

    UPDATA : Guide Mock-Up.

--- front/src/component/Guide.js
+++ front/src/component/Guide.js
@@ -1,39 +1,41 @@
-import React from 'react'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack'
-import { Link } from 'react-router-dom'
+import React from "react"
+import TopHeader from "./TopHeader"
+import Footer from "./Footer"
+import "../css/Guide.css"
+import "../css/Challenge.css"
 
-import '../css/Guide.css'
-function Workout({ history }) {
-  const date = new Date()
-  const goBack = () => {
-    history.goBack()
-  }
-
+function Guide() {
   return (
-    <div className="explainWrap">
-      <div className="explainFlex">
-        <div className="explain">
-          <ArrowBackIcon
-            onClick={goBack}
-            style={{ color: 'white', cursor: 'pointer' }}
-          />
-
-          <p>
-            {date.getFullYear()}년&nbsp;
-            {date.getMonth() + 1}월&nbsp;
-            {date.getDate()}일
-          </p>
-          <p>1. 카메라 권한을 허용해주세요.</p>
-          <p>2. 디바이스를 사용자 앞으로 놓아주세요.</p>
-          <p>3. 화면에 신체가 잘 인식되도록 서주세요.</p>
-          <p>4. '운동하기'를 클릭해 즐거운 운동하세요!</p>
-          <Link to="TeachableMachine">
-            <button>운동하기</button>
-          </Link>
-        </div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <TopHeader />
+      <div className="">
+        <div className="ChallText">메인 페이지</div>
+        <img className="ChallImg" src="./Challengeimg/Flank.jpg" alt="err" />
+        <div className="ChallText">메뉴바</div>
+        <img className="ChallImg" src="./Challengeimg/Pull-up.jpg" alt="err" />
+        <div className="ChallText">SignUp/Login/Logout</div>
+        <img className="ChallImg" src="./Challengeimg/Squat.jpg" alt="err" />
+        <div className="ChallText">My page</div>
+        <img className="ChallImg" src="./Challengeimg/Push-up.jpg" alt="err" />
+        <div className="ChallText">Challenge</div>
+        <img className="ChallImg" src="./Challengeimg/Squat.jpg" alt="err" />
+        <div className="ChallText">Exercise</div>
+        <img className="ChallImg" src="./Challengeimg/Squat.jpg" alt="err" />
+        <div className="ChallText">Products</div>
+        <img className="ChallImg" src="./Challengeimg/Squat.jpg" alt="err" />
+        <div className="ChallText">Support</div>
+        <img className="ChallImg" src="./Challengeimg/Squat.jpg" alt="err" />
       </div>
+      <Footer />
     </div>
   )
 }
 
-export default Workout
+export default Guide
