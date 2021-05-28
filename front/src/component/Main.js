cd3bd9e748aa1da5933a5044c07a8ba0e20a3d24
sangzun-han <hsjun1996@naver.com>
2021-05-29 03:03:53 +0900

    FIX : 글씨체 수정

--- front/src/component/Main.js
+++ front/src/component/Main.js
@@ -10,15 +10,14 @@ import "../css/Main.css"
 
 function Main() {
   const [auth_login, setAuth_login] = useState(false)
-  const [userName, setUserName] = useState('')
-  const [imgPath, setImgPath] = useState('/img/bmi/none.png')
+  const [userName, setUserName] = useState("")
+  const [imgPath, setImgPath] = useState("/img/bmi/none.png")
   const [bmi, setBMI] = useState(0)
   const [height, setHeight] = useState(0)
   const [weight, setWeight] = useState(0)
-  const [myState, setMyState] = useState('')
+  const [myState, setMyState] = useState("")
   const dispatch = useDispatch()
   axios.get(`/user/auth`).then((response) => {
-    
     if (response.data.isAuth) {
       // 로그인한 상태
       let copyAuth = auth_login
@@ -34,41 +33,50 @@ function Main() {
     }
   })
 
-  if(auth_login === true){
-    dispatch(myPage()).then(response => {
+  if (auth_login === true) {
+    dispatch(myPage()).then((response) => {
       response.payload.weight.sort(function (a, b) {
         return a.date < b.date ? -1 : a.date > b.date ? 1 : 0
       })
-      setHeight((response.payload.height/100).toFixed(2))
-      setWeight(response.payload.weight[response.payload.weight.length-1].weight_)
-      if(response.payload.weight.length>0){
-        setBMI((response.payload.weight[response.payload.weight.length-1].weight_/((response.payload.height/100)*(response.payload.height/100))).toFixed(2))
-        if(0<bmi&&bmi<18.5){
-          setImgPath('/img/bmi/1.png')
-          setMyState('저체중')
-        }else if(18.49<bmi&&bmi<25){
-          setImgPath('/img/bmi/2.png')
-          setMyState('표준')
-        }if(24.9<bmi&&bmi<30){
-          setImgPath('/img/bmi/3.png')
-          setMyState('과체중')
-        }if(29.9<bmi&&bmi<35){
-          setImgPath('/img/bmi/4.png')
-          setMyState('비만')
-        }if(34.9<bmi){
-          setImgPath('/img/bmi/5.png')
-        }if(bmi === -1){
-          setImgPath('/img/bmi/all.png')
-          setMyState('고도비만')
+      setHeight((response.payload.height / 100).toFixed(2))
+      setWeight(
+        response.payload.weight[response.payload.weight.length - 1].weight_,
+      )
+      if (response.payload.weight.length > 0) {
+        setBMI(
+          (
+            response.payload.weight[response.payload.weight.length - 1]
+              .weight_ /
+            ((response.payload.height / 100) * (response.payload.height / 100))
+          ).toFixed(2),
+        )
+        if (0 < bmi && bmi < 18.5) {
+          setImgPath("/img/bmi/1.png")
+          setMyState("저체중")
+        } else if (18.49 < bmi && bmi < 25) {
+          setImgPath("/img/bmi/2.png")
+          setMyState("표준")
         }
-      }else{
+        if (24.9 < bmi && bmi < 30) {
+          setImgPath("/img/bmi/3.png")
+          setMyState("과체중")
+        }
+        if (29.9 < bmi && bmi < 35) {
+          setImgPath("/img/bmi/4.png")
+          setMyState("비만")
+        }
+        if (34.9 < bmi) {
+          setImgPath("/img/bmi/5.png")
+        }
+        if (bmi === -1) {
+          setImgPath("/img/bmi/all.png")
+          setMyState("고도비만")
+        }
+      } else {
         setBMI(-1)
       }
     })
-
-
   }
-  
 
   return (
     <div
@@ -83,35 +91,38 @@ function Main() {
       <TopHeader />
       {/* 로그인한 유저 / 로그인 하지 않은 유저 구분 */}
       {auth_login === true ? (
-            <div>
-              <div className="eeeeFlex">
+        <div>
+          <div className="eeeeFlex">
             <div>
               <span>나의 하루 운동 양</span>
             </div>
             <span>
-                <ExerciseData />
+              <ExerciseData />
             </span>
-            </div>
-            <div className="eeeeFlex">
-              <h4>나의 Body Mass Index(BMI)</h4>
-              <p>{weight}(kg) ÷ {height}x{height}(m) = {bmi}(bmi) </p>
-              <p>현재 상태 : {myState} </p>
-              <img src={imgPath} alt="badge" />
-            </div>
-            </div>
-          ) : (
-            <div className="eeeeFlex">
-        <div>
-          <span>로그인하기</span>
+          </div>
+          <div className="eeeeFlex">
+            <p style={{ fontSize: "14px", marginBottom: "10px" }}>
+              나의 Body Mass Index(BMI)
+            </p>
+            <p className="bmiWeight">
+              {weight}(kg) ÷ {height}x{height}(m) = {bmi}(bmi){" "}
+            </p>
+            <p className="myState">현재 상태 : {myState} </p>
+            <img src={imgPath} alt="badge" />
+          </div>
         </div>
-        <span>
+      ) : (
+        <div className="eeeeFlex">
+          <div>
+            <span>로그인하기</span>
+          </div>
+          <span>
             <p>
               <Link to="login">Login</Link>
             </p>
-
-        </span>
-      </div>
-          )}
+          </span>
+        </div>
+      )}
       <div className="eeeeFlex">
         <span className="">오늘의 추천 운동</span>
         <span className="exercise">스쿼트</span>
