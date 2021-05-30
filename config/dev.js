380c1f2bd003630eca9bb30f2338858fac43c238
hanminssssssss <60065661+hanminssssssss@users.noreply.github.com>
2021-05-30 10:00:21 +0900

    fix : path

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://minyim:asd123456@fitsibang.fuqiq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: "mongodb+srv://minyim:asd123456@fitsibang.fuqiq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   }
   
